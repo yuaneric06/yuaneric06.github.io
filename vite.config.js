5e160324d7ebe206862fd2b2f311ef271490f878
Eric Yuan <yuaneric6@gmail.com>
2025-11-19 07:42:00 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/yuaneric06.github.io",
+  base: "/",
   plugins: [
     tailwindcss(),
     react()
